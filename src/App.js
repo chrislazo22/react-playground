83cb91611f8a6cfdcba5facf80abb53f22f9e19f
Christian Lazo <lazochristian.cl@gmail.com>
2019-05-24 21:07:17 -0700

    Add object to index page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  var hellowWorld = 'Welcome to my first React App'
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+	},
+	{
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+	},
+ ];
 
+function App() {
   return (
     <div className="App">
-      <h2>{hellowWorld}</h2>
+      {list.map(item =>
+        <div key={item.objectID}>
+          <span>
+            <a href={item.title}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
